721f7da7536263156ffa99551c4aaf68aa114eb8
Leo Chen <chenlt78@qq.com>
2018-04-28 02:33:08 +0800

    EasyVPN Client BJS test done: Pass
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- EasyVPN_Client_Setup.sh
+++ EasyVPN_Client_Setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -xe
 
 SERVER_IP=$1
-PSK=$2
+VPC_CIDR=$2
+PSK=$3
 
 yum -y install openvpn
 
 #/usr/sbin/openvpn
 
-mkdir -p /data/openvpn/{etc,sbin}
+mkdir -p /data/openvpn/{etc,bin,sbin}
 
 ##
 echo "daemon
@@ -33,15 +34,88 @@ $PSK
 -----END OpenVPN Static key V1----- " > /data/openvpn/etc/static.key
 
 ##
+
+echo "1 vpn" >> /etc/iproute2/rt_tables
+ETH0_IP=`ifconfig eth0 | grep "inet addr" | awk '{print $2}' | cut -d: -f2`
+
+F=/data/openvpn/sbin/startup.sh
 echo "#/bin/bash
-killall openvpn
 /usr/sbin/openvpn --config /data/openvpn/etc/client-static.conf
-exit 0" > /data/openvpn/sbin/startup.sh
+
+## IP Routing
+/sbin/ip rule add from ${VPC_CIDR} table vpn
+/sbin/ip rule add from ${ETH0_IP} table main
+/sbin/ip route flush cache
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+## IPtables
+/sbin/iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
+/sbin/iptables -t nat -A POSTROUTING -o eth+ -j MASQUERADE
+
+## VPN Monitor
+nohup /data/openvpn/sbin/vpn_monitor.sh &
+
+exit 0" > $F
+chmod +x $F
+
+F=/data/openvpn/sbin/vpn_monitor.sh
+echo "#!/bin/bash
+
+LOG=/var/log/vpn_monitor.log
+
+# 2018/04/28
+BJS_SUBNET=\"52.80.0.0/16 52.81.0.0/16 52.94.249.0/28 52.95.255.144/28 54.222.0.0/19 54.222.32.0/22 54.222.36.0/22 54.222.48.0/22 54.222.57.0/24 54.222.58.0/28 54.222.128.0/17 54.223.0.0/16 54.239.0.144/28 54.222.48.0/22 52.80.0.0/16 52.81.0.0/16 52.94.249.0/28 52.95.255.144/28 54.222.32.0/22 54.222.36.0/22 54.222.128.0/17 54.223.0.0/16\"
+ZHY_SUBNET=\"52.82.176.0/22 52.82.180.0/22 52.82.187.0/24 52.82.188.0/22 52.82.192.0/18 52.83.0.0/16 52.94.249.16/28 54.239.0.176/28 52.82.188.0/22 52.82.176.0/22 52.82.180.0/22 52.83.0.0/16 52.94.249.16/28\"
+
+GW=`/sbin/ip route | grep default | awk '{print $3}'`
+
+function do_log()
+{
+    TIME=\`date +\"%Y-%m-%d %T\"\`
+    echo \"\$TIME \$1\" >> \$LOG
+}
+
+function refresh_to_vpn_route()
+{
+    /sbin/ip route add default via 10.8.0.1 table vpn
+	/sbin/ip route add ${VPC_CIDR} via \$GW table vpn
+
+	for N in \${BJS_SUBNET}; do
+		/sbin/ip route add \$N via \$GW table vpn
+	done
+
+	for N in \${ZHY_SUBNET}; do
+		/sbin/ip route add \$N via \$GW table vpn
+	done
+
+    /sbin/ip route fulsh cache
+}
+
+while [ 1 ]; do
+    CHG=0
+
+    if [ \`/sbin/ip route list table vpn  | grep -c 10.8.0.1\` -eq 0 ]; then
+        refresh_to_vpn_route
+        do_log "refresh_to_vpn_route"
+        CHG=1
+    fi
+
+
+    sleep 60
+done
+
+exit 0
+" > $F
+
+chmod +x $F
 
 ##
 echo "/data/openvpn/sbin/startup.sh" >> /etc/rc.local
 
-##
-/usr/sbin/openvpn --config /data/openvpn/etc/client-static.conf
+## First run
+/data/openvpn/sbin/startup.sh
+
+
 
 exit 0
